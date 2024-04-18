4466aa1fe1c8e367e7ea9b60784912b0812b9ef4
Mega-Gorilla <hahahadesu@gmail.com>
2024-04-18 17:52:36 +0900

    設定用pyの作成

---
+++ config.py
@@ -0,0 +1,6 @@
+class config:
+    server_address = "http://127.0.0.1:7860/"
+    preset_prompt = "best quality, masterpiece, ultra-detailed, 8k, high resolution, very aesthetic, absurdres,"
+    preset_ng_prompt = "negativeXL_D,(worst quality:1.4),(low quality:1.4),(monochrome:1.3),(bad anatomy, bad hands:1.4),(watermark, username:1.2),lowres,text,error,missing fingers,extra digit,fewer digits,cropped,normal quality,jpeg artifacts,nsfw,, negativeXL_D,(worst quality:1.4),(low quality:1.4),(monochrome:1.3),(bad anatomy, bad hands:1.4),(watermark, username:1.2),lowres,text,error,missing fingers,extra digit,fewer digits,cropped,normal quality,jpeg artifacts,nsfw,"
+
+    sam_mask_output_path = "./images/output/masks"
