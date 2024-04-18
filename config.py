83fb9fcad0939c36f09c273aa69cceb4037b8fe2
Mega-Gorilla <hahahadesu@gmail.com>
2024-04-18 20:38:31 +0900

    アウトプットパス関数の追加

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class config:
     preset_ng_prompt = "negativeXL_D,(worst quality:1.4),(low quality:1.4),(monochrome:1.3),(bad anatomy, bad hands:1.4),(watermark, username:1.2),lowres,text,error,missing fingers,extra digit,fewer digits,cropped,normal quality,jpeg artifacts,nsfw,, negativeXL_D,(worst quality:1.4),(low quality:1.4),(monochrome:1.3),(bad anatomy, bad hands:1.4),(watermark, username:1.2),lowres,text,error,missing fingers,extra digit,fewer digits,cropped,normal quality,jpeg artifacts,nsfw,"
 
     sam_mask_output_path = "./images/output/masks"
+    output_path = "./images/output"
